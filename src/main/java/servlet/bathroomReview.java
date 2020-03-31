0729fa487991109c4bd4e42dd3aea68d3ff1c654
mrchrisp96 <basket123rocks@gmail.com>
2020-03-30 22:44:48 -0400

    Add files via upload

--- src/main/java/servlet/bathroomReview.java
+++ src/main/java/servlet/bathroomReview.java
@@ -28,7 +28,7 @@ import javax.servlet.http.*;
 //***********************************************************************
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet( name = "twoButtons", urlPatterns = {"/results"} )
+@WebServlet( name = "Assignment6", urlPatterns = {"/assignment6"} )
 public class bathroomReview extends HttpServlet
 {
 
@@ -37,10 +37,6 @@ static String Domain  = "bathroomreviewresults.herokuapp.com";
 static String Path    = "/";
 static String Servlet = "";
 
-// Button labels
-static String OperationAdd = "Add";
-static String OperationSub = "Subtract";
-static String OperationMult = "Multiply";
 
 // Other strings.
 static String Style ="https://www.cs.gmu.edu/~offutt/classes/432/432-style.css";
@@ -55,33 +51,8 @@ static String formHandler = "https://cs.gmu.edu:8443/offutt/servlet/formHandler"
 public void doPost (HttpServletRequest request, HttpServletResponse response)
    throws ServletException, IOException
 {
-   Float rslt   = new Float(0.0);
-   Float lhsVal = new Float(0.0);
-   Float rhsVal = new Float(0.0);
-   String operation = request.getParameter("Operation");
-   String lhsStr = request.getParameter("LHS");
-   String rhsStr = request.getParameter("RHS");
-   if ((lhsStr != null) && (lhsStr.length() > 0))
-      lhsVal = new Float(lhsStr);
-   if ((rhsStr != null) && (rhsStr.length() > 0))
-      rhsVal = new Float(rhsStr);
-
-   if (operation.equals(OperationAdd))
-   {
-      rslt = new Float(lhsVal.floatValue() + rhsVal.floatValue());
-   }
-   else if (operation.equals(OperationSub))
-   {
-      rslt = new Float(lhsVal.floatValue() - rhsVal.floatValue());
-   }
-   else if (operation.equals(OperationMult))
-   {
-      rslt = new Float(lhsVal.floatValue() * rhsVal.floatValue());
-   }
-
    response.setContentType("text/html");
    PrintWriter out = response.getWriter();
-   PrintHead(out);
    PrintBody(out, lhsStr, rhsStr, rslt.toString());
    PrintTail(out);
 }  // End doPost
@@ -95,7 +66,6 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
 {
    response.setContentType("text/html");
    PrintWriter out = response.getWriter();
-   PrintHead(out);
    PrintBody(out);
    PrintTail(out);
 } // End doGet
@@ -103,17 +73,6 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
 /** *****************************************************
  *  Prints the <head> of the HTML page, no <body>.
 ********************************************************* */
-private void PrintHead (PrintWriter out)
-{
-   out.println("<html>");
-   out.println("");
-
-   out.println("<head>");
-   out.println("<title>Two buttons example</title>");
-   out.println(" <link rel=\"stylesheet\" type=\"text/css\" href=\"" + Style + "\">");
-   out.println("</head>");
-   out.println("");
-} // End PrintHead
 
 /** *****************************************************
  *  Prints the <BODY> of the HTML page with the form data
@@ -123,104 +82,140 @@ private void PrintBody (PrintWriter out, String lhs, String rhs, String rslt)
 {
     out.println("<html>");
     out.println("<head>");
-    out.println("<title>");
-    out.println("GMU Bathroom Reviewer");
-    out.println("</title>");
-    out.println("<style type=\"text/css\">");
-    out.println("h1{");
-    out.println("text-align: center;");
-    out.println("<form method=\"post\"");
-    out.println("font-size:1.25em;");
-    out.println("}");
-    out.println("p{");
-    out.println("text-align: center;");
-    out.println("}");
-    out.println("body{");
-    out.println("background-color: lightyellow;");
-    out.println("background-color: lightyellow;");
-    out.println("padding-left: 10em;");
-    out.println("padding-right: 10em;";
-    out.println("}");
-    out.println("form{");
-    out.println("text-align: center;");
-    out.println("}");
-    out.println("</style>");
+    out.println("  <title>");
+    out.println("    GMU Bathroom Reviewer");
+    out.println("  </title>");
+    out.println("  <style type="text/css">");
+    out.println("  h1{");
+    out.println("    text-align: center;");
+    out.println("    font-size:3em;");
+    out.println("  }");
+    out.println("  h3{");
+    out.println("  text-align: center;");
+    out.println("    font-size:1.25em;");
+    out.println("  }");
+    out.println("  p{");
+    out.println("    text-align: center;");
+    out.println("  }");
+    out.println("  body{");
+    out.println("      background-color: lightyellow;");
+    out.println("    padding-left: 10em;");
+    out.println("    padding-right: 10em;");
+    out.println("  }");
+    out.println("  form{");
+    out.println("    text-align: center;");
+    out.println("  }");
+    out.println("  </style>");
     out.println("</head>");
+    out.println("");
     out.println("<body>");
-    out.println("<h1>");
-    out.println("GMU Bathroom Reviewer");
-    out.println("</h1>");
+    out.println("");
+    out.println("<h1>GMU Bathroom Reviewer</h1>");
     out.println("<p>This web app allows you to review the bathrooms of a building at GMU in Fairfax.</p>");
     out.println("<h3>Please select a building.</h3>");
-    out.println("<form method="post" action="+ formHandler + " id="myForm">");
+    out.println("<!-- https://cs.gmu.edu:8443/offutt/servlet/formHandler -->");
+    out.println("<form method="post" action="https://cs.gmu.edu:8443/offutt/servlet/formHandler" id="myForm">");
     out.println("<select name="building">");
-    out.println("<option value="Volgenau" selected="selected">Volgenau School of Engineering</option>");
-    out.println("<option value="Music">Music Theater Building</option>");
-    out.println("<option value="SUB1">Sub1</option>");
-    out.println("<option value="HUB">The Hub</option>");
-    out.println("<option value="deLaski">deLaski Performing Arts Building</option>");
-    out.println("<option value="Planetary">Planetary Hall</option>");
-    out.println("<option value="Innovation">Innovation Hall</option>");
+    out.println("    <option value="Volgenau" selected="selected">Volgenau School of Engineering</option>");
+    out.println("    <option value="Music">Music Theater Building</option>");
+    out.println("    <option value="SUB1">Sub1</option>");
+    out.println("    <option value="HUB">The Hub</option>");
+    out.println("    <option value="deLaski">deLaski Performing Arts Building</option>");
+    out.println("    <option value="Planetary">Planetary Hall</option>");
+    out.println("    <option value="Innovation">Innovation Hall</option>");
+    out.println("");
     out.println("</select>");
+    out.println("");
+    out.println("    <h3>");
+    out.println("        Cleanliness");
+    out.println("    </h3>");
+    out.println("    <p style=“text-align:justify”>");
+    out.println("    <input type="radio" name="clean" id="veryDirty" value="veryDirty" checked/>");
+    out.println("    <label for="veryDirty">Very Dirty</label>");
+    out.println("            <input type="radio" name="clean" id="dirty" value="dirty" />");
+    out.println("    <label for="dirty">Dirty</label>");
+    out.println("            <input type="radio" name="clean" id="slightlyMessy" value="slightlyMessy" />");
+    out.println("    <label for="slightlyMessy">Slightly Messy</label>");
+    out.println("            <input type="radio" name="clean" id="mostlyClean" value="mostlyClean" />");
+    out.println("    <label for="mostlyClean">Mostly Clean</label>");
+    out.println("            <input type="radio" name="clean" id="completelyClean" value="completelyClean" />");
+    out.println("    <label for="completelyClean">Completely Clean</label>");
+    out.println("    </p>");
+    out.println("");
+    out.println("");
+    out.println("    <h3>");
+    out.println("        Odor");
+    out.println("    </h3>");
+    out.println("    <p style=“text-align:justify”>");
+    out.println("    <input type="radio" name="odor" id="unbearable" value="unbearable" checked/>");
+    out.println("        <label for="unbearable">Unbearable</label>");
+    out.println("    <input type="radio" name="odor" id="unpleasant" value="unpleasant" />");
+    out.println("        <label for="unpleasant">Unpleasant</label>");
+    out.println("    <input type="radio" name="odor" id="moderate" value="moderate" />");
+    out.println("        <label for="moderate">Moderate</label>");
+    out.println("    <input type="radio" name="odor" id="unnoticeable" value="unnoticeable" />");
+    out.println("        <label for="unnoticeable">Unnoticeable</label>");
+    out.println("    <input type="radio" name="odor" id="fresh" value="fresh" />");
+    out.println("        <label for="fresh">Fresh</label><br/>");
+    out.println("    </p>");
+    out.println("");
+    out.println("    <h3>");
+    out.println("    Would you use this restroom again?<br/>");
+    out.println("  </h3>");
+    out.println("  <input type="radio" name="wouldUseAgain" id="yes" value="yes" />");
+    out.println("      <label for="yes">Yes</label><br/>");
+    out.println("  <input type="radio" name="wouldUseAgain" id="no" value="no" checked/>");
+    out.println("      <label for="no">No</label><br/>");
+    out.println("");
     out.println("<h3>");
-    out.println("Cleanliness");
+    out.println("    Any additional comments");
     out.println("</h3>");
-    out.println("<p style=“text-align:justify”>");
-    out.println("<input type="radio" name="clean" id="veryDirty" value="veryDirty" checked/>");
-    out.println("<label for="veryDirty">Very Dirty</label>");
-    out.println("<input type="radio" name="clean" id="dirty" value="dirty" />");
-    out.println("<label for="dirty">Dirty</label>");
-    out.println("<input type="radio" name="clean" id="slightlyMessy" value="slightlyMessy" />");
-    out.println("<label for="slightlyMessy">Slightly Messy</label>");
-    out.println("<input type="radio" name="clean" id="mostlyClean" value="mostlyClean" />");
-    out.println("<label for="mostlyClean">Mostly Clean</label>");
-    out.println("<input type="radio" name="clean" id="completelyClean" value="completelyClean" />");
-    out.println("<label for="completelyClean">Completely Clean</label>");
-    out.println("</p>");
-
-
-    out.println("<h3>");
-    out.println("Odor");"
-    out.println("</h3>");"
-    out.println("<p style=“text-align:justify”>");
-    out.println("<input type="radio" name="odor" id="unbearable" value="unbearable" checked/>");
-    out.println("<label for="unbearable">Unbearable</label>");
-    out.println("<input type="radio" name="odor" id="unpleasant" value="unpleasant" />");
-    out.println("<label for="unpleasant">Unpleasant</label>");
-    out.println("<input type="\radio\" name="\odor\" id="\moderate\" value="moderate" />");
-    out.println("<label for="moderate">Moderate</label>");
-    out.println("<input type="radio" name="odor" id="unnoticeable" value="unnoticeable" />");
-    out.println("<label for="unnoticeable">Unnoticeable</label>");
-    out.println("<input type="radio" name="odor" id="fresh" value="fresh" />");
-    out.println("<label for="fresh">Fresh</label><br/>");
-    out.println("</p>");
-
-    out.println("<h3>");
-    out.println("Would you use this restroom again?<br/>");
-    out.println("</h3>");
-    out.println("<input type="\radio\" name="wouldUseAgain" id="yes" value="yes" />");
-    out.println("<label for="yes">Yes</label><br/>");
-    out.println("<input type="radio" name="wouldUseAgain" id="no" value="no" checked/>");
-    out.println("<label for="no">No</label><br/>");
-
-    out.println("<h3>");
-    out.println("Any additional comments");
-    out.println("</h3>");
-                
-    out.println("<td><input type=\"text\" name=\"RHS\" value=\"" + rslt + "\" size=6>");
-
-
+    out.println("");
+    out.println("");
     out.println("<textarea id="userComments" name="userComments" form="myForm" style="width:700px; height:150px;" rows="50" cols="300" placeholder="Enter your message..."></textarea>");
-
-    out.println("<br/><br/>");
-
-
-    out.println("<button onclick="getScore()">Submit</button>");
-    out.println("<p><a href="https://bathroomreviewresults.herokuapp.com">SWE432 Assignment 6</a></p>");
-
-    out.println("<p><a href="https://github.com/mrchrisp96/BathroomReviewResults">GitHub Executable Code</a></p>");
-
+    out.println("");
+    out.println("  <br/><br/>");
+    out.println("");
+    out.println("");
+    out.println("    <button onclick="getScore()">Submit</button>");
+    out.println("    <p><a href="https://bathroomreviewresults.herokuapp.com">SWE432 Assignment 6</a></p>");
+    out.println("");
+    out.println("    <p><a href="https://github.com/mrchrisp96/BathroomReviewResults">GitHub Executable Code</a></p>");
     out.println("</form>");
+    out.println("");
+    out.println("<script>");
+    out.println("function doGet() {");
+    out.println("    ");
+    out.println("}");
+    out.println("function getScore(){");
+    out.println("   var i = 0;");
+    out.println("   var odor = -1;");
+    out.println("   var clean = -1;");
+    out.println("   var would = 0;");
+    out.println("   let cIDs = ["veryDirty","dirty","slightlyMessy","mostlyClean","completelyClean"];");
+    out.println("   let oIDs = ["unbearable","unpleasant","moderate","unnoticeable","fresh"];");
+    out.println("   while (i<5){");
+    out.println("     if (document.getElementById(cIDs[i]).checked){");
+    out.println("       clean = i;");
+    out.println("     }");
+    out.println("     if (document.getElementById(oIDs[i]).checked){");
+    out.println("       odor = i;");
+    out.println("     }");
+    out.println("     i = i + 1;");
+    out.println("   }");
+    out.println("   if (document.getElementById("yes").checked){");
+    out.println("     would = 1;");
+    out.println("   }");
+    out.println("");
+    out.println("   let score = (12 * clean) + (8 * odor) + (would * 20);");
+    out.println("");
+    out.println("   window.alert("Restroom review score: " + score + " out of 100");");
+    out.println("}");
+    out.println("</script>");
+    out.println("");
+    out.println("</body>");
+    out.println("</html>");
+
     
 } // End PrintBody
 
@@ -238,7 +233,7 @@ private void PrintBody (PrintWriter out)
 ********************************************************* */
 private void PrintTail (PrintWriter out)
 {
-   out.println("</body>");
+   out.println("");
    out.println("</html>");
 } // End PrintTail
 
